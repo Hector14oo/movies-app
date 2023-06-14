df7df441f09c64039d2ff76c09bf9e237d27ce20
Hector14oo <devtor140017@gmail.com>
2023-06-14 17:04:40 -0500

    V_Login: add prop placeholder for the input component and his instances in Container_Login

--- src/containers/Container_Login.tsx
+++ src/containers/Container_Login.tsx
@@ -4,6 +4,7 @@ import { Button, Input } from '@components';
 import { Login } from 'assets/figures';
 
 import styles from '@styles/containers/Login.module.css';
+
 export function Container_Login() {
   const handleClick = () => {
     alert('Login');
@@ -15,8 +16,14 @@ export function Container_Login() {
         height={250}
       />
       <form action=''>
-        <Input type='EMAIL' />
-        <Input type='PASSWORD' />
+        <Input
+          type='EMAIL'
+          placeholder='Email'
+        />
+        <Input
+          type='PASSWORD'
+          placeholder='Password'
+        />
         <Link href={'/account/forgot-password'}>Forgot Password?</Link>
         <Button
           type='NORMAL'
@@ -24,7 +31,7 @@ export function Container_Login() {
           handler={handleClick}
         />
         <span>
-          New here? <Link href={'/account/forgot-password'}>Register</Link>
+          New here? <Link href={'/account/register'}>Register</Link>
         </span>
       </form>
     </main>
