5ff07911bbcf2de3a925b8c1498ca8910892b1c8
Hector14oo <devtor140017@gmail.com>
2023-06-14 17:09:49 -0500

    Revert "V_Login: add prop placeholder for the input component and his instances in Container_Login"
    This reverts commit df7df441f09c64039d2ff76c09bf9e237d27ce20.

--- src/containers/Container_Login.tsx
+++ src/containers/Container_Login.tsx
@@ -4,7 +4,6 @@ import { Button, Input } from '@components';
 import { Login } from 'assets/figures';
 
 import styles from '@styles/containers/Login.module.css';
-
 export function Container_Login() {
   const handleClick = () => {
     alert('Login');
@@ -16,14 +15,8 @@ export function Container_Login() {
         height={250}
       />
       <form action=''>
-        <Input
-          type='EMAIL'
-          placeholder='Email'
-        />
-        <Input
-          type='PASSWORD'
-          placeholder='Password'
-        />
+        <Input type='EMAIL' />
+        <Input type='PASSWORD' />
         <Link href={'/account/forgot-password'}>Forgot Password?</Link>
         <Button
           type='NORMAL'
@@ -31,7 +24,7 @@ export function Container_Login() {
           handler={handleClick}
         />
         <span>
-          New here? <Link href={'/account/register'}>Register</Link>
+          New here? <Link href={'/account/forgot-password'}>Register</Link>
         </span>
       </form>
     </main>
