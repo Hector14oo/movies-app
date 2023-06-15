eb688777d1645745436a1a93891d540c1e4c3b5a
Hector14oo <devtor140017@gmail.com>
2023-06-15 17:30:01 -0500

    All input components are separated

---
+++ src/components/Input/Email/index.tsx
@@ -0,0 +1,24 @@
+import { At } from 'assets/icons';
+import styles from '@styles/components/Inputs.module.css';
+
+export function Email({
+  placeholder,
+}: {
+  placeholder: 'Email' | 'Confirm Email';
+}) {
+  return (
+    <label
+      htmlFor={placeholder}
+      className={styles.FormInput}
+    >
+      <i>
+        <At />
+      </i>
+      <input
+        type='email'
+        id={placeholder}
+        placeholder={placeholder}
+      />
+    </label>
+  );
+}
