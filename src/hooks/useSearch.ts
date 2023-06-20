fac2d68478c2bf4181ceaf4f986e6eb2ca17eb7c
Hector14oo <devtor140017@gmail.com>
2023-06-20 09:50:27 -0500

    useSearch hook created

---
+++ src/hooks/useSearch.ts
@@ -0,0 +1,46 @@
+import { searchEndPoint } from '@utils/constants';
+import { fecthApi } from '@utils/fetchApi';
+import { useEffect, useState } from 'react';
+
+interface ResultType {
+  id: string;
+  release_date: string;
+  title: string;
+  overview: string;
+  vote_average: number;
+  poster_path: string;
+}
+
+interface MovieType {
+  id: number;
+  date: string;
+  title: string;
+  overview: string;
+  rank: number;
+  poster: string;
+}
+
+export function useSearch(value: string) {
+  const [movies, setMovies] = useState<MovieType[]>([]);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      fecthApi(searchEndPoint(value)).then(({ results }) => {
+        setMovies(
+          results.map((movie: ResultType) => ({
+            id: movie.id,
+            date: movie.release_date,
+            title: movie.title,
+            overview: movie.overview,
+            rank: movie.vote_average.toFixed(1),
+            poster: movie.poster_path,
+          }))
+        );
+      });
+    }, 500);
+
+    return () => clearTimeout(timer);
+  }, [value]);
+
+  return movies;
+}
