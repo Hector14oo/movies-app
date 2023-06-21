1649cb693db65205946038440778a6e20d7117e1
Hector14oo <devtor140017@gmail.com>
2023-06-21 18:46:11 -0500

    Home view update

--- src/containers/Home.tsx
+++ src/containers/Home.tsx
@@ -1,30 +1,44 @@
 import { useTrending } from '@hooks/useTrending';
 import { CardPoster, CardTrailer } from '@components';
+import { ErrorFigure } from 'assets/figures';
 
 import styles from '@styles/containers/Home.module.css';
 
 export async function Home() {
-  const trending = await useTrending();
+  const { result, error } = await useTrending();
   return (
-    <main className={styles.Main}>
-      <h1>Trending Movies Of The Week</h1>
-      <section>
-        {trending.splice(0, 2).map(({ id, title, date, votes, poster }) => (
-          <CardPoster
-            key={id}
-            id={id}
-            title={title}
-            date={date}
-            votes={votes}
-            poster={poster}
+    <>
+      {error ? (
+        <main className={styles.MainError}>
+          <ErrorFigure
+            width={300}
+            height={250}
           />
-        ))}
-      </section>
-      <h2>Lastest Trailers</h2>
-      <section>
-        <CardTrailer title='Super Mario Bros' />
-        <CardTrailer title='Super-Man' />
-      </section>
-    </main>
+          <h1>{"We're sorry, an error has occurred :("}</h1>
+          <p>{error.toString()}, try again</p>
+        </main>
+      ) : (
+        <main className={styles.Main}>
+          <h1>Trending Movies Of The Week</h1>
+          <section>
+            {result?.splice(0, 2).map(({ id, title, date, votes, poster }) => (
+              <CardPoster
+                key={id}
+                id={id}
+                title={title}
+                date={date}
+                votes={votes}
+                poster={poster}
+              />
+            ))}
+          </section>
+          <h2>Lastest Trailers</h2>
+          <section>
+            <CardTrailer title='Super Mario Bros' />
+            <CardTrailer title='Super-Man' />
+          </section>
+        </main>
+      )}
+    </>
   );
 }
