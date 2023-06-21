acd47241906b6201f0e692cbd2d84b4353287bb4
Hector14oo <devtor140017@gmail.com>
2023-06-21 18:57:31 -0500

    Update search

--- src/containers/Search.tsx
+++ src/containers/Search.tsx
@@ -3,14 +3,13 @@
 import { useState, ChangeEvent } from 'react';
 import { CardPoster, InputSearcher } from '@components';
 import { useSearch } from '@hooks/useSearch';
-import { SearchFigure } from 'assets/figures';
+import { ErrorFigure, SearchFigure } from 'assets/figures';
 
 import styles from '@styles/containers/Search.module.css';
 
 export function Search() {
   const [value, setValue] = useState('');
-  const { movies, error } = useSearch(value);
-  const className = error ? 'FLEX' : movies.length ? 'GRID' : 'FLEX';
+  const { movies, noResults, error } = useSearch(value);
 
   const handleOnChange = (e: ChangeEvent<HTMLInputElement>) => {
     setValue(e.target.value);
@@ -29,37 +28,48 @@ export function Search() {
         value={value}
         reset={() => setValue('')}
       />
-      <section className={styles[className]}>
-        {error ? (
-          <>
-            <SearchFigure />
-            <h1>No results found</h1>
-          </>
-        ) : (
-          <>
-            {movies.length ? (
-              <>
-                {movies.map(({ id, date, title, overview, votes, poster }) => (
-                  <CardPoster
-                    key={id}
-                    id={id}
-                    date={date || 'Unknown date'}
-                    title={shorTitle(title)}
-                    overview={overview}
-                    votes={votes}
-                    poster={poster}
-                  />
-                ))}
-              </>
-            ) : (
-              <>
-                <SearchFigure />
-                <h1>The movies you search for will appear here.</h1>
-              </>
-            )}
-          </>
-        )}
-      </section>
+      {error.state ? (
+        <section className={styles.FLEX}>
+          <ErrorFigure
+            width={300}
+            height={250}
+          />
+          <h1>{"We're sorry, an error has occurred :("}</h1>
+          <p>{error.error?.toString()}, try again</p>
+        </section>
+      ) : (
+        <>
+          {movies.length === 0 && !noResults && (
+            <section className={styles.FLEX}>
+              <SearchFigure />
+              <h1>The movies you search for will appear here.</h1>
+            </section>
+          )}
+
+          {movies.length > 0 && !noResults && (
+            <section className={styles.GRID}>
+              {movies.map(({ id, date, title, overview, votes, poster }) => (
+                <CardPoster
+                  key={id}
+                  id={id}
+                  date={date || 'Unknown date'}
+                  title={shorTitle(title)}
+                  overview={overview}
+                  votes={votes}
+                  poster={poster}
+                />
+              ))}
+            </section>
+          )}
+
+          {noResults && (
+            <section className={styles.FLEX}>
+              <SearchFigure />
+              <h1>No results found</h1>
+            </section>
+          )}
+        </>
+      )}
     </main>
   );
 }
