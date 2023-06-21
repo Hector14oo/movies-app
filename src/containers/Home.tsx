4adfd13ecdcd6870994fc01e481a4182726718e4
Hector14oo <devtor140017@gmail.com>
2023-06-21 12:46:51 -0500

    Trailers are connected to API

--- src/containers/Home.tsx
+++ src/containers/Home.tsx
@@ -2,9 +2,13 @@ import { useTrending } from '@hooks/useTrending';
 import { CardPoster, CardTrailer } from '@components';
 
 import styles from '@styles/containers/Home.module.css';
+import { useTrailers } from '@hooks/useTrailers';
 
 export async function Home() {
   const trending = await useTrending();
+  const trailers = await useTrailers();
+
+  console.log(trailers);
   return (
     <main className={styles.Main}>
       <h1>Trending Movies Of The Week</h1>
@@ -22,8 +26,15 @@ export async function Home() {
       </section>
       <h2>Lastest Trailers</h2>
       <section>
-        <CardTrailer title='Super Mario Bros' />
-        <CardTrailer title='Super-Man' />
+        {trailers.splice(0, 2).map(({ id, videoKey, title, backdrop_path }) => (
+          <CardTrailer
+            key={id}
+            id={id}
+            videoKey={videoKey}
+            title={title}
+            backdrop={backdrop_path}
+          />
+        ))}
       </section>
     </main>
   );
