c11577b6cdf58aac5e93e89ae2f5c760900dae85
Hector14oo <devtor140017@gmail.com>
2023-06-21 12:48:55 -0500

    TrailerCard connected with api, change width of the img in PosterCard, and update styles

--- src/components/Card/Trailer/index.tsx
+++ src/components/Card/Trailer/index.tsx
@@ -1,12 +1,44 @@
+'use client';
+
+import { useState } from 'react';
+import { TrailerType } from '@interfaces';
 import { Play } from 'assets/icons';
 
 import styles from '@styles/components/Cards.module.css';
 
-export function Trailer({ title }: { title: string }) {
+export function Trailer({ id, videoKey, title, backdrop }: TrailerType) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  let img = `https://image.tmdb.org/t/p/original${backdrop}`;
+
+  if (backdrop === null || backdrop === undefined)
+    img = 'assets/Images/BrokenImage.png';
   return (
-    <article className={styles.TrailerCard}>
-      <Play width={26} height={30}/>
-      <h2>{title} - Official Trailer</h2>
+    <article
+      className={styles.TrailerCard}
+      onClick={() => setIsOpen(!isOpen)}
+      style={{
+        background: `var(--GRADIENT), url(${img}) no-repeat top/cover`,
+      }}
+    >
+      <Play
+        width={26}
+        height={30}
+      />
+      <h2>{title} | Official Trailer</h2>
+      {isOpen && (
+        <figure className={styles.TrailerModal}>
+          <div>
+            <iframe
+              src={`https://www.youtube.com/embed/${videoKey}?disablekb=1&rel=0&autoplay=1`}
+              title={title}
+              frameBorder='0'
+              allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+              allowFullScreen
+            />
+          </div>
+        </figure>
+      )}
     </article>
   );
 }
