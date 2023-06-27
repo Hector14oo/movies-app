6073a3e68482ec5b00abddc44a37a6293655e03c
Hector14oo <devtor140017@gmail.com>
2023-06-27 09:52:21 -0500

    update icons

--- public/assets/icons/AboutUsSvg.tsx
+++ public/assets/icons/AboutUsSvg.tsx
@@ -9,12 +9,12 @@ export function AboutUsSvg({
       width={width}
       height={height}
       viewBox='0 0 38 38'
-      fill='none'
+      fill='inherit'
       xmlns='http://www.w3.org/2000/svg'
     >
       <path
         d='M19 0C8.5234 0 0 8.5234 0 19C0 29.4766 8.5234 38 19 38C29.4766 38 38 29.4766 38 19C38 8.5234 29.4766 0 19 0ZM20.9 28.5H17.1V17.1H20.9V28.5ZM20.9 13.3H17.1V9.5H20.9V13.3Z'
-        fill='#EEEEEE'
+        fill='inherit'
       />
     </svg>
   );
