e83b26664638a4ec74cbd965464b1570b01c9ca2
Hector14oo <devtor140017@gmail.com>
2023-07-13 16:37:27 -0500

    Added toFixed() to votes

--- src/components/Rank/index.tsx
+++ src/components/Rank/index.tsx
@@ -2,14 +2,14 @@ import styles from '@styles/components/Rank.module.css';
 
 export function Rank({ votes }: { votes: number }) {
   let color = 'var(--BAD)';
-  if (votes > +4) color = 'var(--MEH)';
+  if (votes > 4) color = 'var(--MEH)';
   if (votes >= 7) color = 'var(--GOOD)';
   return (
     <span
       className={styles.Span}
       style={{ backgroundColor: color }}
     >
-      {votes}
+      {votes.toFixed(1)}
     </span>
   );
 }
