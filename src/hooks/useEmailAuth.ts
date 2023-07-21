0aa60215a197ad1b75dc8621593f965664de172d
Hector14oo <devtor140017@gmail.com>
2023-07-20 22:22:17 -0500

    useEmailAuth update

---
+++ src/hooks/useEmailAuth.ts
@@ -0,0 +1,36 @@
+'use client';
+
+import { useState } from 'react';
+import {
+  createUserWithEmailAndPassword,
+  fetchSignInMethodsForEmail,
+  signInWithEmailAndPassword,
+} from 'firebase/auth';
+import { auth } from '@firebase/client';
+
+export function useEmailAuth() {
+  const [error, setError] = useState('');
+
+  const logInWithEmail = async (email: string, password: string) => {
+    setError('');
+
+    try {
+      const singMethod = await fetchSignInMethodsForEmail(auth, email);
+      if(singMethod.includes('google.com')) throw new Error('User must login with Google.')
+    } catch (err) {
+      if (err instanceof Error) setError(err.message);
+    }
+  };
+
+  const registerWithEmail = async (email: string, password: string) => {
+    setError('');
+
+    try {
+      await createUserWithEmailAndPassword(auth, email, password);
+    } catch (err) {
+      if (err instanceof Error) setError(err.message);
+    }
+  };
+
+  return { logInWithEmail, registerWithEmail, error };
+}
