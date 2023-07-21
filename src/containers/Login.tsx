c1d2fffb38b1efac7bd2bdc3943e5a91fb99edfa
Hector14oo <devtor140017@gmail.com>
2023-07-20 22:23:08 -0500

    Finish login and register logic

--- src/containers/Login.tsx
+++ src/containers/Login.tsx
@@ -10,17 +10,27 @@ import {
   InputEmail,
   InputPassword,
 } from '@components';
-import { LoginFigure } from 'assets/figures';
 
 import { useGoogleAuth } from '@hooks/useGoogleAuth';
+import { useEmailAuth } from '@hooks/useEmailAuth';
+import { useSessionContext } from '@context/SessionContext';
+
+import { LoginFigure } from 'assets/figures';
 
 import styles from '@styles/containers/Login.module.css';
-import { useSessionContext } from '@context/SessionContext';
+
+const message = [
+  'Firebase: Error (auth/user-not-found)',
+  'Firebase: Error (auth/wrong-password)',
+  'User must login with Google.'
+];
 
 export function Login() {
   const { user } = useSessionContext();
   const { push } = useRouter();
   const { logInWithGoogle } = useGoogleAuth();
+  const { logInWithEmail, error } = useEmailAuth();
+
   const [isBlind, setIsBlind] = useState(false);
 
   useEffect(() => {
@@ -28,7 +38,18 @@ export function Login() {
     push('/');
   }, [user, push]);
 
-  const handleClick = (e: MouseEvent<HTMLButtonElement>) => {
+  const handleSubmit = (e: MouseEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const email = formData.get('email')?.toString();
+    const password = formData.get('password')?.toString();
+
+    if (!email || !password) return;
+
+    logInWithEmail(email, password);
+  };
+
+  const handleButtonGoogle = (e: MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
     logInWithGoogle();
   };
@@ -39,23 +60,26 @@ export function Login() {
   return (
     <main className={styles.Login}>
       <LoginFigure />
-      <form>
+      <form onSubmit={handleSubmit}>
         <h1>Login</h1>
         <InputEmail placeholder='Email' />
+        {error.includes(message[0]) && <span className={styles.Error}>The email is not registered.</span>}
         <InputPassword
           isBlind={isBlind}
           toggleBlind={toggleBlind}
           placeholder='Password'
         />
-        <Link href={'/account/forgot-password'}>Forgot Password?</Link>
+        {error.includes(message[1]) && <span className={styles.Error}>The password is incorrect.</span>}
+        <Link href={'/forgot-password'}>Forgot Password?</Link>
+        {error.includes(message[2]) && <span className={styles.Error}>{message[2]}</span>}
         <ButtonNormal
           text='Login'
-          onClick={handleClick}
+          onClick={() => {}}
         />
         <span>
-          New here? <Link href={'/account/register'}>Register</Link>
+          New here? <Link href={'/register'}>Register</Link>
         </span>
-        <ButtonGoogle onClick={handleClick} />
+        <ButtonGoogle onClick={handleButtonGoogle} />
       </form>
     </main>
   );
