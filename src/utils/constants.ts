ab2d440f052531f81df88169eaa3f530a86cef52
Hector14oo <devtor14@gmail.com>
2024-01-12 11:55:56 -0500

    :pencil2: Fix a typo in the search endpoint

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -7,7 +7,7 @@ export const OPTIONS = {
 
 const AUTH = `api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}`
 
-export const SEARCH_ENDPOINT = (value: string) => `https://api.themoviedb.org/3/search/movie?${AUTH}query=${value}&page=1`;
+export const SEARCH_ENDPOINT = (value: string) => `https://api.themoviedb.org/3/search/movie?api_key=711c3953b76ca0680b4475fae04db9d3&query=${value}&page=1`;
 
 export const TRENDING_ENDPOINT = `https://api.themoviedb.org/3/trending/movie/week?${AUTH}`;
 
